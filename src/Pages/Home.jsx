5d944dd21bd79ace6d4d8ef1a22fd337d6c54373
VittoDambro00 <vittodambro00@gmail.com>
2023-12-13 14:26:06 +0100

    Cambiamento home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,24 @@
 import Axios from "axios";
 import PokeCard from "../Components/PokeCard";
+import { useState } from "react";
 
 export default function Home() {
   let endpoint = "https://pokeapi.co/api/v2/pokemon/";
-  const [endUpdate, setEndUpdate] = useState("")
+  const [endUpdate, setEndUpdate] = useState("");
 
+  function getPokemon() {
+    Axios.get(endpoint + { endUpdate }).then((res) => {
 
-  function getPokemon(){ Axios.get(endpoint + {endUpdate}).then((res) => {
-        
-  });
-}
+    });
+  }
+
   return (
     <div className="bg-slate-800 flex flex-col items-center justify-center gap-4">
       <h1 className="text-slate-50">Capturalos todos!</h1>
-      <input type="text" className=""  onChange={(event)=>setEndUpdate(event.target.value)}/>
+      <input
+        type="text"
+        onChange={(event) => setEndUpdate(event.target.value)}
+      />
       <button className="border-2 border-slate-50 bg-slate-400">
         Busca a tu pokémon!
       </button>
